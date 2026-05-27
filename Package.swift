4b6f06379b67f819fa835d1a0e4a1a236e8414d4
澁田謙太 <33084602+KentaShibuta@users.noreply.github.com>
2026-05-27 20:42:28 +0900

    Add Package.swift for MyOpenCV library

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MyOpenCV",
+    products: [
+        .library(name: "opencv2", targets: ["opencv2"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "opencv2",
+            url: "https://api.github.com/repos/KentaShibuta/MyOpenCV/releases/assets/429882551.zip", // .zip必須
+            checksum: "71ccbaf3a4e3068de32b126acd2d5f2cc51e434e1691454058fd8709ef6e0b8e"
+        ),
+    ]
+)
